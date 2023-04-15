03a7538ff7bbbe368001dab1e7cd94938ff137e4
sudokzt <wato9902@gmail.com>
2023-04-16 02:17:02 +0900

    chore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,20 @@ import "@/styles/gcse-style.css";
 
 import type { AppProps } from "next/app";
 
-import { configureChains, WagmiConfig, createClient, useAccount } from "wagmi";
+import { configureChains, WagmiConfig, createClient } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { goerli } from "@wagmi/core/chains";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { Footer } from "@/components/FooterTab";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { publicEnv } from "../env";
 
-const { chains, provider, webSocketProvider } = configureChains(
+const { provider, webSocketProvider } = configureChains(
   [goerli],
   [publicProvider()]
 );
 const client = createClient({
   autoConnect: true,
-  connectors: [new MetaMaskConnector({ chains })],
+  connectors: [],
   provider,
   webSocketProvider,
 });
