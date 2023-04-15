f755cd293c204ec92751d560ecffcae0ab6013a7
sudokzt <wato9902@gmail.com>
2023-04-16 07:22:15 +0900

    chore

--- pages/explorer/index.tsx
+++ pages/explorer/index.tsx
@@ -17,7 +17,7 @@ const CardItem: React.FC<CardItemProps> = ({
 }) => {
   return (
     <a
-      href={`explorer/${link}`}
+      href={link}
       className="bg-white rounded-md shadow-md p-4 flex items-center space-x-4 hover:shadow-lg transform hover:scale-105 transition-all duration-200 ease-in-out"
     >
       <Image
@@ -57,26 +57,60 @@ const Explorer = () => {
 
   const items = [
     {
+      imageSrc: "/lens.png",
+      title: "LENS",
+      description:
+        "An open-source protocol that allows developers to create decentralized applications (dApps) on the Ethereum blockchain.",
+      link: "https://www.lens.xyz/",
+    },
+    {
+      imageSrc: "/topshot.jpeg",
+      title: "NBA Top Shot",
+      description:
+        "A blockchain-based platform for buying, selling, and trading officially licensed NBA collectibles in the form of digital moments.",
+      link: "https://nbatopshot.com/",
+    },
+    {
       imageSrc: "/uniswap.jpeg",
-      title: "UniSwap",
-      description: "This is a sample description for the first card.",
-      link: "uniswap",
+      title: "Uniswap",
+      description:
+        "A decentralized exchange that enables the swapping of cryptocurrencies without the need for an intermediary.",
+      link: "https://uniswap.org/",
+    },
+    {
+      imageSrc: "/sandbox.jpeg",
+      title: "SANDBOX",
+      description:
+        "A virtual world where users can build, buy, and sell virtual assets.",
+      link: "https://www.sandbox.game/jp/",
     },
     {
       imageSrc: "/opensea.png",
       title: "Opensea",
-      description: "This is a sample description for the second card.",
-      link: "opensea",
+      description:
+        "A marketplace for buying and selling digital assets, including non-fungible tokens (NFTs) and other unique digital items.",
+      link: "https://opensea.io/ja",
     },
   ];
 
   return (
-    <div className="max-w">
+    <div className="max-w mb-24">
       <MobileHeader title="Explorer" />
-      <div className="max-w-7xl mx-auto px-4 py-8">
+      <div className="max-w-7xl mx-auto px-4 py-4">
+        <span className="text-xl font-semibold text-gray-800">
+          Recommendations just for you
+        </span>
+        <div className="mt-2"></div>
         <CardList items={items} />
       </div>
-      <div className="gcse-search" />
+      <hr className="my-2 mx-2 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
+      <div className="mx-auto px-4">
+        <span className="text-xl font-semibold text-gray-800">
+          Search based on token graphs
+        </span>
+        <div className="mt-2"></div>
+        <div className="gcse-search" />
+      </div>
     </div>
   );
 };
