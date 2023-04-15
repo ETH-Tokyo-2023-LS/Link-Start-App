e2942a71b4c2cfc255b373c195816d5352cd8033
sudokzt <wato9902@gmail.com>
2023-04-16 08:22:38 +0900

    chore

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -150,15 +150,15 @@ export default function Wallet() {
     <Stack w="100%" alignItems="center" justifyContent="center">
       <MobileHeader />
       <Stack
-        // spacing="4"
-        // maxW="1000px"
-        // minW={["100%", "600px"]}
+        spacing="4"
+        maxW="1000px"
+        minW={["100%", "600px"]}
         p={["6", "10"]}
         bg="whtie"
       >
         <Stack
           spacing="8"
-          // p="6"
+          p="6"
           borderRadius="lg"
           border="2px"
           borderColor="blackAlpha.50"
